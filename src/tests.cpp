f3890c977035009f9d5891e12ed64656bef72531
Sebastien Rubens <90621822+seb-pg@users.noreply.github.com>
2023-01-05 10:42:22 +0000

    Update tests.cpp

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,8 +1,8 @@
 // openmsg by Sebastien Rubens
 //
 // To the extent possible under law, the person who associated CC0 with
-// openbmp has waived all copyright and related or neighboring rights
-// to openbmp.
+// openmsg has waived all copyright and related or neighboring rights
+// to openmsg.
 //
 // You should have received a copy of the CC0 legalcode along with this
 // work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
